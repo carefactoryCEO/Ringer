4070b739c0f8f4515d43723050cde27a62c7101a
mobum Shin <mbshin@carefactory.co.kr>
2019-11-26 22:08:22 +0900

    return not found when log in failed

--- Ringer.HubServer/Controllers/AuthController.cs
+++ Ringer.HubServer/Controllers/AuthController.cs
@@ -6,30 +6,34 @@ using Ringer.HubServer.Extensions;
 
 namespace Ringer.HubServer.Controllers
 {
-	[Route("[controller]")]
-	[ApiController]
-	public class AuthController : ControllerBase
-	{
-		private readonly RingerDbContext _dbContext;
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly RingerDbContext _dbContext;
 
-		public AuthController(RingerDbContext dbContext)
-		{
-			_dbContext = dbContext;
-		}
+        public AuthController(RingerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
-		[HttpPost("login")]
-		public ActionResult<string> Login(LoginInfo loginInfo)
-		{
-			User user = _dbContext.Users.FirstOrDefault(u =>
-				u.Name == loginInfo.Name &&
-				u.BirthDate.Date == loginInfo.BirthDate.Date &&
-				u.Gender == loginInfo.Gender
-			);
+        [HttpPost("login")]
+        public ActionResult<string> Login(LoginInfo loginInfo)
+        {
+            User user = _dbContext.Users.FirstOrDefault(u =>
+                u.Name == loginInfo.Name &&
+                u.BirthDate.Date == loginInfo.BirthDate.Date &&
+                u.Gender == loginInfo.Gender
+            );
 
-			// TODO: DB에서 일치하는 user를 찾지 못했을 때
-			// 새 user로 등록? 에러 throw?
+            // DB에 정보 없음.
+            if (user == null)
+                return NotFound("notFound");
 
-			return Ok(user.JwtToken());
-		}
-	}
+            // TODO: DB에서 일치하는 user를 찾지 못했을 때
+            // 새 user로 등록? 에러 throw?
+
+            return Ok(user.JwtToken());
+        }
+    }
 }
